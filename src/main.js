f172f27008dbd0eb0d42ace5397f60f4a832fb23
lyy <liyy@wise2c.com>
2019-11-28 16:10:05 +0800

    放出console.log

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-// import VConsole from "vconsole";
+import VConsole from "vconsole";
 // 视频播放器
 import VideoPlayer from "vue-video-player";
 require("video.js/dist/video-js.css");
 require("vue-video-player/src/custom-theme.css");
 Vue.use(VideoPlayer);
-// if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-//   const vConsole = new VConsole();
-//   Vue.use(vConsole);
-// }
+if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+  const vConsole = new VConsole();
+  Vue.use(vConsole);
+}
 Vue.config.productionTip = false;
 router.beforeEach((to, from, next) => {
   if (to.meta.title) {
